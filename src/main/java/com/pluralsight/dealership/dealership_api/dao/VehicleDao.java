aa8e664eeae87e82d78ca29bf919ef84f3a08959
Ayanna Williams <ayanna.williams20@att.net>
2024-12-13 15:58:14 -0500

    added vehicle dao class

---
+++ src/main/java/com/pluralsight/dealership/dealership_api/dao/VehicleDao.java
@@ -0,0 +1,13 @@
+package com.pluralsight.dealership.dealership_api.dao;
+
+import java.util.List;
+
+public interface VehicleDao {
+    boolean addVehicle(Vehicle vehicle);
+    List<Vehicle> findAllVehicles();
+    List<Vehicle> findVehicleByPrice(double minPrice, double maxPrice);
+    List<Vehicle> findVehicleByMakeModel(String make, String model);
+    List<Vehicle> findVehicleByYear(int year);
+    List<Vehicle> findVehicleByColor(String color);
+    boolean deleteVehicle(String vin);
+}
